f007df72101e3cd8dccd2d5ed4c563723d337f3a
Ostanin Kirill <grant1337@pm.me>
2019-04-26 16:38:40 +0400

    v1.0

---
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -0,0 +1,54 @@
+package edu.epam.izhevsk.junit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.AdditionalMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class PaymentControllerTest {
+
+
+    @Mock
+    private AccountService accountService;
+
+    @Mock
+    private DepositService depositService;
+
+    @InjectMocks
+    private PaymentController paymentController = new PaymentController(accountService, depositService);
+
+
+    @Before
+    public void setUp() throws Exception {
+        when(accountService.isUserAuthenticated(100L)).thenReturn(true);
+        when(depositService.deposit(AdditionalMatchers.gt(100L), anyLong())).thenThrow(new InsufficientFundsException());
+    }
+
+
+    @Test
+    public void testUserIdSuccess() throws InsufficientFundsException {
+        paymentController.deposit(50L, 100L);
+        verify(accountService, times(1)).isUserAuthenticated(100L);
+
+    }
+
+    @Test(expected = SecurityException.class)
+    public void failedDepositUnauthUser() throws InsufficientFundsException {
+        paymentController.deposit(49L, 105L);
+    }
+
+    @Test(expected = InsufficientFundsException.class)
+    public void test() throws InsufficientFundsException {
+        paymentController.deposit(150L, 100L);
+    }
+
+
+}
